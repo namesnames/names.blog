3cde4f3ba83d867126cf064f4a5eb2eb701a65ef
namesnames <hanju@ihanjuui-MacBookAir.local>
2023-06-25 23:19:27 +0900

    first

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "names",
+  description: "Backend Developer",
+  author: "names",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/namesnames",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:97gkswn@gmail.com",
+  },
+  utterances: {
+    repo: "devHudi/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
